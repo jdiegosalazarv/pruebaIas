b0b2f904044e5770720770cba3339adea95d8062
Juan Diego Salazar Velasquez <judsalaz@bancolombia.com.co>
2024-05-25 11:49:50 -0500

    project started

---
+++ src/test/java/co/com/ias/pruebaias/PruebaIasApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.ias.pruebaias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaIasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
